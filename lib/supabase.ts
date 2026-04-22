d8de436d0bb7a92d692ce78f2ccde782bacf7131
Richardo09 <rikardobarus015@gmail.com>
2026-04-23 05:48:18 +0700

    fix: move supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,11 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// URL project kamu
-const supabaseUrl = 'https://ksocdwbjybounpspyhyg.supabase.co';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-// ANON PUBLIC KEY 
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtzb2Nkd2JqeWJvdW5wc3B5aHlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyNDUyMTIsImV4cCI6MjA5MDgyMTIxMn0.NsE7m3Bc_1yVicnOQsDOZbdtunqmt0Xn0ATl2yzz5d0';
-
-// buat koneksi
 export const supabase = createClient(supabaseUrl, supabaseKey);
